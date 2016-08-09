72a7d6a344ae009e8183720cb0c7d75c01e391ee
bkilrain <bkilrain@gmail.com>
2016-08-08 19:29:38 -0700

    Implement room selection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,31 +2,30 @@ let app = {};
 
 $(document).ready(function() {
   let d = new Date();
-  
-  app.init = function() {
-    let fetchInterval = window.setInterval( () => app.fetch(), 1000);
-  };
-
-  app.send = function() {
+  app.createMessage = function() {
     let $text = $('.messageInput');
-    debugger;
     let message = {
       username: 'Meow',
       text: $text.val(),
       roomname: '4chan'
     };
+    app.send(message);
+  };
+  
+  app.init = function() {
+    let fetchInterval = window.setInterval( () => app.fetch(), 1000);
+  };
 
+  app.send = function(message) {
     $.ajax({
-    // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        $text.val('');
+        $('.messageInput').val('');
       },
       error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
     });
@@ -36,7 +35,7 @@ $(document).ready(function() {
     // console.log('fetch is running');
     let parseData = function(jsonObj) {
       for (let i = 0; i < jsonObj.results.length; i++) {
-        addToDOM(jsonObj.results[i]);
+        app.addMessage(jsonObj.results[i]);
       }
     };
 
@@ -47,9 +46,12 @@ $(document).ready(function() {
 
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: `https://api.parse.com/1/classes/messages?order=-createdAt&where={"createdAt":{"$gte":{"__type":"Date","iso":"${d.toISOString()}"}}}`,
+      url: 'https://api.parse.com/1/classes/messages',
       type: 'GET',
-      // data: JSON.stringify(message),
+      data: {
+        'order': '-createdAt',
+        'where': `{"createdAt":{"$gte":{"__type":"Date","iso":"${d.toISOString()}"}}}`
+      },
       contentType: 'application/json',
       success: function (data) {
         parseData(data);
@@ -61,6 +63,23 @@ $(document).ready(function() {
       }
     });
   };
+
+  app.clearMessages = function() {
+    $('#chats').children().remove();
+  };
+
+  app.addMessage = function(data) {
+    let $text = $('<p></p>').text(`${new Date(data.createdAt)} Username: ${data.username} Message: ${data.text}`);
+    $('#chats').prepend($text);
+  };
+
+  app.addRoom = function(data) {
+    let roomName = ($('.roomSelectText')).val();
+    let $room = $('<option></option>').text(roomName).val(roomName);
+    $('#roomSelect').append($room);
+  };
+
+  app.server = 'https://api.parse.com/1/classes/messages';
 
   app.init();  
 });
