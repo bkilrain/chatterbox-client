ed55afb4b3c546e58bd2b2eff0d31882c2cb5b35
bkilrain <bkilrain@gmail.com>
2016-08-08 17:25:59 -0700

    Implement send functionality
    Add text box and submit button.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,29 @@
-$(document).ready(function() {
-  let d = new Date(0);
-  let app = {};
+let app = {};
 
+$(document).ready(function() {
+  let d = new Date();
+  
   app.init = function() {
     let fetchInterval = window.setInterval( () => app.fetch(), 1000);
   };
 
   app.send = function() {
+    let $text = $('.messageInput');
+    debugger;
+    let message = {
+      username: 'Meow',
+      text: $text.val(),
+      roomname: '4chan'
+    };
+
     $.ajax({
     // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/messages',
       type: 'POST',
-      data: JSON.stringify('message111111'),
+      data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log(data);
+        $text.val('');
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -24,6 +33,7 @@ $(document).ready(function() {
   };
 
   app.fetch = function() {
+    // console.log('fetch is running');
     let parseData = function(jsonObj) {
       for (let i = 0; i < jsonObj.results.length; i++) {
         addToDOM(jsonObj.results[i]);
@@ -31,13 +41,13 @@ $(document).ready(function() {
     };
 
     let addToDOM = function(data) {
-      let $text = $('<p></p>').text(`Username: ${data.username} Message: ${data.text}`);
+      let $text = $('<p></p>').text(`${new Date(data.createdAt)} Username: ${data.username} Message: ${data.text}`);
       $('#chats').prepend($text);
     };
 
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: `https://api.parse.com/1/classes/messages?where={"createdAt":{"$gte":{"__type":"Date","iso":"${d.toISOString()}"}}}`,
+      url: `https://api.parse.com/1/classes/messages?order=-createdAt&where={"createdAt":{"$gte":{"__type":"Date","iso":"${d.toISOString()}"}}}`,
       type: 'GET',
       // data: JSON.stringify(message),
       contentType: 'application/json',
@@ -47,10 +57,10 @@ $(document).ready(function() {
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
+        console.error('chatterbox: Failed to fetch message', data);
       }
     });
   };
-  
+
   app.init();  
 });
